186d01a01db32f880eb544e29e8e4ed209c83dff
byeongwoonlee <tfh100@gmail.com>
2022-04-27 16:37:33 +0900

    root Reducer 뼈대 구축

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, Store } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/CounterSlice";
+import reducer from "../rootReducer";
 
 const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-    },
+    reducer,
 });
 
 export type AppDispatch = typeof store.dispatch;
